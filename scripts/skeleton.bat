3787695c6ba90b4da0399726a21291995fe77e12
Lindsay Evans <linz@linz.id.au>
2012-10-05 10:43:09 +1000

    Making type & name user configurable from command line

--- scripts/skeleton.bat
+++ scripts/skeleton.bat
@@ -8,7 +8,8 @@ REM 	@ECHO %~nx0 "My Awesome Program"
 REM 	GOTO end
 
 REM :build
-	nant -buildfile:%~dp0skeleton.build -D:project-name=%1
+nant -buildfile:%~dp0skeleton.build -D:project-type=%1 -D:project-fullname=%2
+REM echo nant -buildfile:%~dp0skeleton.build -D:project-type=%1 -D:project-fullname=%2
 
 REM :end
 
